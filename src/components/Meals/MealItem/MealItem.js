ab8adaa4b5912bb4f4333cbf95b3ecd756ff2c29
wjdals95 <wjdals992@naver.com>
2023-01-16 15:54:41 +0900

    portal 사용해서 모달 추가하기

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,8 @@
 import React from "react";
+import MealItemForm from "./MealItemForm";
+
 import classes from './MealItem.module.css'
+
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
@@ -9,7 +12,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm  id={props.id} />
+      </div>
     </li>
   );
 };
